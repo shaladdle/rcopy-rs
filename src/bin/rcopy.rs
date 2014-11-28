d16388978d22e08edc1671aa6c10d3a500ec3fe4
Adam Wright <adam.austin.wright@gmail.com>
2014-11-28 18:45:55 -0500

    Removed _path from path variable names

--- src/bin/rcopy.rs
+++ src/bin/rcopy.rs
@@ -9,7 +9,7 @@ use std::io::FileStat;
 use std::io::{TypeFile, TypeDirectory};
 
 fn main() {
-    let (src_dir_path, dst_dir_path) = match os::args()[] {
+    let (src_dir, dst_dir) = match os::args()[] {
         [_, ref s, ref d] => (Path::new(s), Path::new(d)),
         _ => {
             println!("usage: rcopy src_dir dst_dir");
@@ -17,16 +17,16 @@ fn main() {
         },
     };
 
-    let mut elems = match fs::walk_dir(&src_dir_path) {
+    let mut elems = match fs::walk_dir(&src_dir) {
         Ok(x) => x,
         Err(e) => {
-            println!("Could not walk directory \"{}\": {}", src_dir_path.display(), e);
+            println!("Could not walk directory \"{}\": {}", src_dir.display(), e);
             std::os::set_exit_status(-1);
             return;
         }
     };
-    for src_file_path in elems {
-        let file_size = match fs::stat(&src_file_path) {
+    for src_file in elems {
+        let file_size = match fs::stat(&src_file) {
             Ok(info) =>  {
                 if info.kind == TypeDirectory {
                     continue;
@@ -34,29 +34,29 @@ fn main() {
                 info.size
             },
             Err(e) => {
-                println!("Couldn't stat file \"{}\" while walking: {}", src_file_path.display(), e);
+                println!("Couldn't stat file \"{}\" while walking: {}", src_file.display(), e);
                 continue;
             },
         };
-        let rel_file_path = match src_file_path.path_relative_from(&src_dir_path) {
+        let rel_file = match src_file.path_relative_from(&src_dir) {
             Some(p) => p,
             None => {
-                println!("Couldn't get relative path for \"{}\" relative to \"{}\"", src_file_path.display(), src_dir_path.display());
+                println!("Couldn't get relative path for \"{}\" relative to \"{}\"", src_file.display(), src_dir.display());
                 std::os::set_exit_status(-1);
                 return;
             },
         };
-        let dst_file_path = dst_dir_path.join(&rel_file_path);
+        let dst_file = dst_dir.join(&rel_file);
 
         // If the file already exists and it has the right file size, assume it was copied
         // properly.
         //
         // TODO: what if there is a progress file there? Do we remove it? That's kind of an
         // implementation detail :(.
-        match fs::stat(&dst_file_path) {
+        match fs::stat(&dst_file) {
             Ok(FileStat{size: existing_file_size, ..}) => {
                 if existing_file_size == file_size {
-                    println!("[ {}/{} ] {} (skipped)", file_size, file_size, rel_file_path.display());
+                    println!("[ {}/{} ] {} (skipped)", file_size, file_size, rel_file.display());
                     continue;
                 }
             },
@@ -64,16 +64,16 @@ fn main() {
         }
 
         // Create the containing directory for the destination file if it doesn't exist.
-        let dst_file_dir_path = dst_file_path.dir_path();
-        match fs::stat(&dst_file_dir_path) {
+        let dst_file_dir = dst_file.dir_path();
+        match fs::stat(&dst_file_dir) {
             Ok(FileStat{kind: TypeFile, ..}) => {
-                println!("Want \"{}\" to be a directory, but it already exists as a file", dst_file_dir_path.display());
+                println!("Want \"{}\" to be a directory, but it already exists as a file", dst_file_dir.display());
                 std::os::set_exit_status(-1);
                 return;
             }
             Err(_) => {
-                if let Err(e) = fs::mkdir(&dst_file_dir_path, std::io::USER_DIR) {
-                    println!("Couldn't create destination file's directory \"{}\": {}", dst_file_dir_path.display(), e);
+                if let Err(e) = fs::mkdir(&dst_file_dir, std::io::USER_DIR) {
+                    println!("Couldn't create destination file's directory \"{}\": {}", dst_file_dir.display(), e);
                     std::os::set_exit_status(-1);
                     return
                 }
@@ -82,11 +82,11 @@ fn main() {
         }
 
         // Start the async copy
-        let progress_rx = rcopy::resumable_file_copy(&dst_file_path, &src_file_path);
+        let progress_rx = rcopy::resumable_file_copy(&dst_file, &src_file);
 
         // Wait for the copy to be complete, printing progress as it goes
         for progress in progress_rx.iter() {
-            print!("\r[ {}/{} ] {}", progress.current, progress.total, rel_file_path.display());
+            print!("\r[ {}/{} ] {}", progress.current, progress.total, rel_file.display());
         }
         print!("\n");
     }
